97d32f2beb496d32bda22c6fc6c68fa2ba1738e6
KyleTiley <contact@kyletiley.com>
2023-11-24 14:13:53 +0200

    grenade spawns need tweaking

---
+++ Enemies/BombSpawner.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+var spawn_locations = []
+var spawn_time = 3
+var time_count = spawn_time
+var grenade = load("res://Enemies/grenade.tscn")
+
+func _ready():
+	for child in self.get_children():
+		spawn_locations.append(child)
+
+func _physics_process(delta):
+	time_count += delta
+	if time_count > spawn_time:
+		time_count = 0
+		var new_grenade = grenade.instantiate()
+		get_parent().add_child(new_grenade)
+		new_grenade.position = spawn_locations[random_number_generator(0, spawn_locations.size())].position
+
+func random_number_generator(_min, _max):
+	var rng = RandomNumberGenerator.new()
+	var new_random_number = rng.randf_range(_min, _max)
+	return new_random_number
--- Enemies/Grenade.gd
+++ Enemies/Grenade.gd
@@ -1,11 +1,5 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	await get_tree().create_timer(4).timeout
+	print("EXPLODE")
