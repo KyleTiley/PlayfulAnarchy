9a06d019c426e08896fc5b1b29fe181c8c689fb5
Kyle Tiley <kylestiley@gmail.com>
2023-11-25 15:10:25 +0200

    flip gate added

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,6 +5,8 @@ var move_speed = 200
 var turn_speed = 4
 var facing = -1
 var count = 0 
+var flip_gate = 0.5
+var flip_counter = flip_gate
 
 func _physics_process(delta):
 	if !game_started:
@@ -12,16 +14,21 @@ func _physics_process(delta):
 	move_local_x(-move_speed*delta)
 	player_movement(delta)
 	count += delta
+	flip_counter += delta
 	if count > 0.5:
 		Global.score += 1
 		count = 0
 
 func player_movement(_delta):
 	if Input.is_action_pressed("left_input"):
+		if flip_counter < flip_gate:
+			return
 		if facing == -1:
 			return
 		flip_player()
 	if Input.is_action_pressed("right_input"):
+		if flip_counter < flip_gate:
+			return
 		if facing == 1:
 			return
 		flip_player()
@@ -41,6 +48,7 @@ func _on_area_2d_area_entered(area):
 func flip_player():
 	scale.x = -scale.x
 	facing = -facing
+	flip_counter = 0
 
 func _on_game_start_game():
 	game_started = true
